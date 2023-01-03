f3c69df82ec7998d8555716d2f53b5e4692a5233
matswiderski <matswi18ii@gmail.com>
2023-01-03 15:18:10 +0100

    repair history

--- src/components/pages/repairs/Repairs.js
+++ src/components/pages/repairs/Repairs.js
@@ -1,14 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "../../../api/axios";
 import usePage from "../../hooks/usePage";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { Typography } from "@mui/material";
 import RepairSteps from "./RepairSteps";
+import RecentRepairs from "./RecentRepairs";
+import { Typography } from "@mui/material";
 function Repair(props) {
   const page = usePage();
+  const [repairs, setRepairs] = useState([]);
+
   useEffect(() => {
     page.setCurrentPageName(props.pageName);
   }, [page.currentPageName]);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const getRepairs = await axios({
+          method: "get",
+          url: "repair/get-all",
+          headers: { "Content-Type": "application/json" },
+        });
+        setRepairs(getRepairs.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
   return (
     <Grid container item spacing={2} sx={{ display: "flex" }}>
       <Grid item xs={12}>
@@ -20,12 +39,18 @@ function Repair(props) {
             borderRadius: "4px",
           }}
         >
-          <RepairSteps />
+          <RepairSteps repairs={repairs} setRepairs={setRepairs} />
         </Box>
       </Grid>
-      <Grid item xs={12}></Grid>
-      <Grid item xs={12}></Grid>
-      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <Typography
+          variant="h6"
+          sx={{ my: 1, fontWeight: "500", letterSpacing: "1px" }}
+        >
+          Recent repairs
+        </Typography>
+        <RecentRepairs repairs={repairs}/>
+      </Grid>
     </Grid>
   );
 }
